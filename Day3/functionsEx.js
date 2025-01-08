541d33f5606435a39c1782f878f793b096be5528
siddu780 <siddubanki70@gmail.com>
2025-01-08 11:52:30 +0530

    first commit

---
+++ Day3/functionsEx.js
@@ -0,0 +1,16 @@
+//! Without Parameters
+// function demo(){
+//     console.log("Hello Iam function");
+// }
+// demo();//~function Calling
+
+//! With parameters and arguments
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(3,4)//~function calling
+
+function addToCart(){
+    document.body.style.background="blue";
+}
---
+++ Day3/looping.js
@@ -0,0 +1,36 @@
+//!For Loop
+//* Without updater(i++), loop goes infinite.
+// for(let i = 0; i <= 5; i++){
+//     console.log("Hello World!")
+// }
+
+//~even or odd
+// for(let i = 0;i < 100;i++){
+//     if(i%2==0){
+//         console.log(i+" is even.")
+//     }else{
+//         console.log(i+" is odd")
+//     }
+// }
+
+//~ Write a JavaScript Program to print a factorial of a number.
+// let factorial=1;
+// for(let i=0;i<5;i++){
+//     factorial+=factorial*i;
+// }
+// console.log(factorial)
+
+//! While loop
+// let i = 1;
+// while(i<=5){
+//     console.log("Good Morning");
+//     i++;
+// }
+
+//! do-while
+// let i=1;
+// do{
+//     console.log("Please wake up")
+//     i++;
+// }while(i<=5);
+
