f3760a6180dd82ad41bd191af20d3611ae69f21e
siddu780 <siddubanki70@gmail.com>
2025-01-10 13:19:44 +0530

    first commit

---
+++ Day4/Assignment.js
@@ -0,0 +1,28 @@
+//& Square
+// for(let i=0;i<5;i++){
+//     let row="";
+//     for(let j=0;j<5;j++){
+//         row+=" * ";
+//     }
+//     console.log(row)
+// }
+//&Left Triangle
+// for(let i=0;i<5;i++){
+//     let row="";
+//     for(j=0;j<=i;j++){
+//         row+=" * ";
+//     }
+//     console.log(row)
+// }
+//& x
+// for(let i=0;i<5;i++){
+//     let row="";
+//     for(let j=0;j<5;j++){
+//         if(j==i||i+j==4){
+//             row+=" * ";
+//         }else{
+//             row+="   "
+//         }
+//     }
+//    console.log(row)
+// }
