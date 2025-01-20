243ca6fe64357475497330db3ddaa20040a8a65c
siddu780 <siddubanki70@gmail.com>
2025-01-20 10:31:37 +0530

    first commit

---
+++ Day7/example.js
@@ -0,0 +1,20 @@
+// let userDetails = {
+//     name:"name1",
+//     age:23,
+//     city:"Hyd"
+// }
+
+//javascript object
+// console.log(userDetails)
+// console.log(userDetails.name)
+// console.log(typeof(userDetails))
+
+//JSON object
+// let x = JSON.stringify(userDetails)
+// console.log(x.name)
+// console.log(x)
+// console.log(typeof(x))
+
+// let y = JSON.parse(x)
+// console.log(y)
+// console.log(y.name)
