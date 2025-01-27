3435c34fc9f688a4856e447b29189222476b67c6
siddu780 <siddubanki70@gmail.com>
2025-01-27 14:10:11 +0530

    first commit

---
+++ Day12/dom1.js
@@ -0,0 +1,8 @@
+let x = document.createElement("h1")
+x.innerText = "Hi Hello"
+x.setAttribute("id","demo")
+document.body.appendChild(x)
+
+let y = document.createElement("img")
+y.src = "https://www.google.com/search?sca_esv=6c20ea1ff241b1df&q=images&udm=2&fbs=ABzOT_AqAj1UoYQh8kglFXNyluWOqa5Byjz8gYDQhQlAIuFG_RXyZCeU2CGLlRRY-8c8ThOompI0geecy3YALCAf5ZhHV6qua5jl204OPWnAiPvz98ok7zT4f_-_bXFp1Lft1Ggp2UDbk0G52StrQlo7imUThSZXQsrQKoxwfXI2iHETNAnAZ3ooR3d40u45zarzf42CVp7JD37YMH2ZMoYH2qRAf3Jw9Q&sa=X&ved=2ahUKEwj3j5rtiZCLAxWWUGwGHWW7GRoQtKgLegQIHBAB&cshid=1737780910328149&biw=1536&bih=695&dpr=1.25#vhid=O60LdRXewHbkcM&vssid=mosaic"
+document.body.appendChild(y)
---
+++ Day13/dom.js
@@ -0,0 +1,80 @@
+//! document.getElementByID()
+
+// let element = document.getElementById("demo")
+// element.innerText = "DOM";
+// console.log(element)
+
+// let div = document.getElementById("test");
+// // div.innerText="<h1>Header</h1>"
+// div.innerHTML="<h1>Header</h1>"
+// console.log(div)
+
+// let ele = document.getElementsByClassName("test");
+// console.log(ele)
+// console.log(Array.isArray(ele))
+// ele[0].style.backgroudColor="yellow"
+//!spread operator:- it is used to takeout each value from the original array and add it in  new array(pure array)
+//!syntax:-[...variable]
+// let x  = [...ele]
+// console.log(Array.isArray(x))
+// x.map((ele)=>{
+//     ele.style.backgroudColor="blue"
+// })
+    
+//!querySelector
+//!querySelectorAll
+    
+// let ele = document.querySelector("#demo")
+// console.log(ele)
+// let ele2 = document.querySelector(".test")
+// console.log(ele2)
+
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele)
+// [...ele].map(ele=>{
+//     console.log(ele.innerText)
+// })\
+
+//!events
+
+// let btn = document.getElementById("btn")
+// syntax:- button.addEventListener("events",callback function)
+// btn.addEventListener("click",()=>{
+    //     console.log("hi")
+    // })
+// btn.addEventListener("dblclick",()=>{
+//     document.body.style.backgroundColor="teal"
+// })
+// btn.addEventListener("mouseleave",()=>{
+//     alert("mouse leave")
+// })
+// btn.addEventListener("mouseenter",()=>{
+//     alert("mouse enter")
+// })
+// btn.addEventListener("mouseover",()=>{
+//     alert("mouse over")
+// })
+// let btn2 = document.getElementById("btn2")
+
+// btn2.addEventListener("keydown",()=>{
+//     alert("key down ")
+// })
+// btn2.addEventListener("keyup",()=>{
+//     alert("key down ")
+// })
+
+// let btns = document.getElementsByClassName("color");
+
+// [...btns].map((element)=>{
+//     element.addEventListener("mouseover",()=>{
+//         element.style.backgroundColor=element.innerText;
+//     })
+//     element.addEventListener("mouseleave",()=>{
+//         element.style.backgroundColor="transparent";
+//     })
+//     element.addEventListener("mouseenter",()=>{
+//         element.style.backgroundColor=element.innerText;
+//     })
+// })
+
